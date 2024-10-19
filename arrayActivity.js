f17721ffac6d39e523bd29899aa04ce9150d069c
chntlg <chantalguerrero04@gmail.com>
2024-10-20 01:53:51 +0800

    Initial commit with arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,13 @@
+
+let favSingers = ["Taylor Swift", "Ed Sheeran", "Sarah Geronimo"];
+//array index notation
+console.log(favSingers[0]);
+
+let favNumbers = ["10", "04", "01","23"];
+
+let mixedArr = ["string",["otherarray"],123,true];
+//Access and log each item in the mixedArr using array index notation.
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
